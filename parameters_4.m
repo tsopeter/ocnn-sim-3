d3f52bb9db874020c03e8e066771b323da984061
Peter Tso <tsopeter@ku.edu>
2023-06-05 11:59:51 -0500

    Update parameters_4.m
    Added comments describing what each value does

--- parameters_4.m
+++ parameters_4.m
@@ -1,38 +1,43 @@
 %parameters 4
 
-%parameters_3
-
-% parameters-2
-%
-% parameters
+% Parameters 4 is based on the following paramter format
+% - parameters_3
+% - parameters-2
+% - parameters
 
 %
 % note that the base measurement are
-% mW (Watts)
+% mW (milliWatts)
 % M (Meters)
 
-Nx     = 512;
+Nx     = 512;               % size of whatever screen is used
 Ny     = 512;
-ratio  = 1;
+ratio  = 1;                 % ratio factor
 ix     = round(Nx/ratio);
 iy     = round(Ny/ratio);
+
 nx     = 21.16e-3;          % 21.16 mm, assumed size of DLP
 ny     = 21.16e-3;
-d1     = 20e-2;             % distance 20 cm
-d2     = 20e-2;
-wv     = 400e-9;
-a0     = 20;
-r1     = nx/6;
+d1     = 1.4e-3;            % distance of 1.4 mm, this is the focal
+d2     = 1.4e-3;            % length of the SAM (Saturable Absorbption Mirror)
+wv     = 1550e-9;           % use 1550 nm wavelength
+a0     = 20;                % NOT USED
+
+r1     = nx/6;              % specifies detection zone and size  
 r2     = nx/30;
+
 rate   = 1;
 lvalue = 1e-6;
-sx     = 2;
+
+sx     = 2;                 % not used (used or modified tanh function)
 sy     = 1;
 sc     = 0.45;
 sz     = 1/6;
-P      = 1;     % default power of 1 mW
-mm     = 2 * 10e3;
-probability = 0.25;
+
+P      = 1;                 % default power of 1 mW
+mm     = 2 * 10e3;          % For use in DLPEndLayer. Larger the factor, means sharper response
+probability = 0.25;         % For use in DLPLayer. This factor allows dithering in 1-bit DLPs
+IPower = 8;                 % start with 8 mW
 
 filenameImagesTrain = 'Images/train-images-idx3-ubyte.gz';
 filenameLabelsTrain = 'Images/train-labels-idx1-ubyte.gz';
@@ -45,9 +50,9 @@ dataTest  = create_imagedatastore(filenameLabelsTest , "DLPTestImagesPNG/");
 %dataTrain = create_xor_imagedatastore("XORLabels/train_labels.mat", "TrainImagesXOR");
 %dataTest  = create_xor_imagedatastore("XORLabels/test_labels.mat" , "TestImagesXOR");
 
-numEpochs = 64;
-miniBatchSize = 128;
-learnRate = (1e-4);
+numEpochs = 64;             % number of epochs
+miniBatchSize = 128;        % size of batches per epoch
+learnRate = (1e-4);         % learn rate (reduce when NaNs occur)
 
 dataTest  = partition(dataTest , 10 , 1);
 Sx = 256;
@@ -67,4 +72,4 @@ k = min(kx, ky);
 w1 = fftshift(get_propagation_distance(Nx, Ny, nx, ny, d1, wv));
 w2 = fftshift(get_propagation_distance(Nx, Ny, nx, ny, d2, wv));
 
-kernel = internal_random_amp(Nx, Ny);
+kernel = internal_random_amp(Nx, Ny)*IPower;        % create the kernel
