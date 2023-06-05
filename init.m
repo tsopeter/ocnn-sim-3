a919ed4dc8283889aeed2e15294a9015dd545f0c
Peter Tso <tsopeter@ku.edu>
2023-06-05 16:19:00 -0500

    Cleaned up file tree

---
+++ init.m
@@ -0,0 +1,10 @@
+% init
+
+% initialize all needed paths
+addpath(genpath('Utils'));
+addpath(genpath('Layers'));
+addpath(genpath('hdl'));
+
+% initalize all networks
+addpath(genpath('cnns'));
+addpath(genpath('cnn_sam'));
